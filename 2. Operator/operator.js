a3535e4742809aec0e6c67f26cc9cc1fb976feb0
BukTon <buk_ton2@hotmail.com>
2019-09-29 12:17:31 +0700

    oparator

---
+++ 2. Operator/operator.js
@@ -0,0 +1,26 @@
+//  +, -, *, /
+
+let num = 0
+
+
+num += 1
+num -= 1
+
+num++
+
+let str = ""
+
+str+="test" + num
+
+str = `number ${num}`
+
+console.log(num, str)
+
+let a = 1
+let b = 3
+
+const total = eval("a + b")
+
+console.log(total)
+
+const s = ""
